214fc65fb05b2229dd47e11d3a70852ee7bd0df2
kjabbar256@gmail.com <jabbarkashif798@gmail.com>
2023-08-15 13:14:15 +0500

    comment

--- Components/Map/Map.jsx
+++ Components/Map/Map.jsx
@@ -76,7 +76,8 @@ export default function Map() {
   <br />
   <br />
   <br />
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3401.329801790362!2d74.25313237452433!3d31.515100647404385!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3919037618082ee1%3A0x483a02c7d20a4bfe!2sDunitSol!5e0!3m2!1sen!2s!4v1691667708707!5m2!1sen!2s" width="98%" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+{/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3401.329801790362!2d74.25313237452433!3d31.515100647404385!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3919037618082ee1%3A0x483a02c7d20a4bfe!2sDunitSol!5e0!3m2!1sen!2s!4v1691667708707!5m2!1sen!2s" width="98%" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
+<iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3715.0421642742467!2d39.87936147526665!3d21.38824438035246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjHCsDIzJzE3LjciTiAzOcKwNTInNTUuMCJF!5e0!3m2!1sen!2s!4v1692087025593!5m2!1sen!2s" width="98%" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </Grid>
 </Grid>
 <br />
