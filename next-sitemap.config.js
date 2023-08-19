fd9a6c626a9f2907eda0ceaf496f50ea1a61bef8
kjabbar256@gmail.com <jabbarkashif798@gmail.com>
2023-08-19 13:44:07 +0500

    comment

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  siteUrl: 'https://www.subhanallahumrahtaxi.com/', // Replace with your website URL
+  generateRobotsTxt: true, // Optional: Generate a robots.txt file
+  exclude: ['/admin', '/admin/*'], // Optional: Exclude specific paths from the sitemap
+};
